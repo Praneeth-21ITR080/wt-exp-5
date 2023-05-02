9c691981812b7ebbd35a8044624913c56fef89ea
Praneeth-21ITR080 <118598882+Praneeth-21ITR080@users.noreply.github.com>
2023-05-02 22:54:37 +0530

    Add files via upload

---
+++ post.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const qs = require('querystring');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const formData = qs.parse(body);
+      res.writeHead(600, {'Content-Type': 'text/html'});
+      console.log(`Name: ${formData.fname}\nEmail: ${formData.email} \nPhone number: ${formData.phn} \n your password:${formData.pwd} `);
+      res.end(`Name: ${formData.fname}\nEmail: ${formData.email} \nPhone number: ${formData.phn} \n your password:${formData.pwd} `);
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`
+      not correct
+    `);
+  }
+});
+
+server.listen(3001, () => {
+  console.log('Server running on port 3000');
+});
